661f0ba7de1496a5a05ee4a5d9cfb35032966192
alyshae <alison.walas@gmail.com>
2017-07-15 10:25:38 -0700

    added validations in user model for name, email & password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   has_many :stories
   has_many :photos, through: :stories
 
+  REGEX_VALID_EMAIL = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true
+  validates :password, presence: true,
+    length: {minimum: 3}
+  validates :email, presence: true,
+    uniqueness: true,
+    length: {maximum: 60},
+    format: {with: REGEX_VALID_EMAIL}
+
   def self.confirm params
     @user = User.find_by({email: params[:email]})
     @user ? @user.authenticate(params[:password]) : false
