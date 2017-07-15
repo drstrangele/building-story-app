1c859348357b0f72d1646b6f826da186999e421e
alyshae <alison.walas@gmail.com>
2017-07-15 10:34:43 -0700

    created sign-up form view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
