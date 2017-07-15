28900ab3baa331ea71d8b90ce5fd31d032e9be0c
alyshae <alison.walas@gmail.com>
2017-07-15 12:57:10 -0700

    On building index (root) page, made each building name a link_to directing to the associated building show page.

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -6,8 +6,7 @@ class BuildingsController < ApplicationController
 
   def show
     @building = Building.find_by_id(params[:id])
-    @stories = Story.find_by(params[:building_id])
   end
-  
+
 
 end
