29d065f03262ee2434442e2a4359e979971a857b
alyshae <alison.walas@gmail.com>
2017-07-15 10:19:51 -0700

    created all necessary actions in the sessions controller including user_params as a private action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.confirm(user_params)
+    if @user
+      #create sessionsHelper to login & call it here on @user
+      #flash notice - successful login
+      redirect_to root_path
+    else
+      #flash error - login failed
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    #create sessions to logout the user & call it here
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
