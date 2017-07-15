f21690b29e5022622501e9baf14be65e200ae840
alyshae <alison.walas@gmail.com>
2017-07-15 10:14:28 -0700

    created the confirm method in order to authenticate that a user's email exists in the database and they have entered the correct password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :stories
   has_many :photos, through: :stories
+
+  def self.confirm params
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+  end
 end
