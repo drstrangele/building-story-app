82542124543bc0ba7ead3f89e2947bf786a0d82a
alyshae <alison.walas@gmail.com>
2017-07-15 17:56:37 -0700

    added edit user view & link_to this view on app layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.update_attributes(user_params)
+      login(@user)
+      #flash notice - user info updated successfully
+      redirect_to root_path
+    else
+      #flash error - updates not saved
+      redirect_to edit_user_path(@user)
+    end
+  end
 
   private
   def user_params
