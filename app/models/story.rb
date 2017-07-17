7a6272cf1e55363c17f4acddb9c987ce9d5f58a6
nuranned <nuranned@gmail.com>
2017-07-17 12:20:56 -0700

    working on forms styling

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,8 @@ class Story < ApplicationRecord
   belongs_to :user
   belongs_to :building
   has_many :photos
+
+  validates :title, presence: true, length: { maximum: 200, allow_nill: false, message: "Can't be nil" }
+  validates :description, presence: true
+
 end
